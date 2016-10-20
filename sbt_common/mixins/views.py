6fac1a8915845877b7592b994ce9265ef4d64531
Nuno Silva <nmsilva@ubiwhere.pt>
2016-10-20 17:12:15 +0100

    create CustomAuthenticationViewMixin for custom authentication actions

---
+++ sbt_common/mixins/views.py
@@ -0,0 +1,12 @@
+from authentication import UserIsAuthenticated
+
+
+class CustomAuthenticationViewMixin(object):
+    auth_actions = None
+    permission_classes = []
+
+    # http://stackoverflow.com/a/30624527
+    def get_permissions(self):
+        if self.action in self.auth_actions:
+            self.permission_classes = [UserIsAuthenticated, ]
+        return super(CustomAuthenticationViewMixin, self).get_permissions()
