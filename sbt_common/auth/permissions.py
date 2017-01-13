9899800ce320c39d481d5a23e686c2de850b99d2
Nuno Silva <nmsilva@ubiwhere.pt>
2017-01-13 17:20:53 +0000

    support for permissions - dynamic permission model name

--- sbt_common/auth/permissions.py
+++ sbt_common/auth/permissions.py
@@ -5,16 +5,28 @@ from rest_framework.permissions import DjangoModelPermissions
 class ServiceModelPermissions(DjangoModelPermissions):
 
     perms_map = {
-        'GET': ['list_%(model_name)s'],
+        'GET': ['%(model_name)s.view'],
         'OPTIONS': [],
         'HEAD': [],
-        'POST': ['add_%(model_name)s'],
-        'PUT': ['change_%(model_name)s'],
-        'PATCH': ['change_%(model_name)s'],
-        'DELETE': ['delete_%(model_name)s'],
+        'POST': ['%(model_name)s.add'],
+        'PUT': ['%(model_name)s.edit'],
+        'PATCH': ['%(model_name)s.edit'],
+        'DELETE': ['%(model_name)s.remove'],
     }
 
+    def get_required_permissions(self, method, model_cls):
+        """
+        Given a model and an HTTP method, return the list of permission
+        codes that the user is required to have.
+        """
+        model_name = getattr(self._view, 'model_permissions', model_cls._meta.model_name)
+        kwargs = {
+            'model_name': model_name
+        }
+        return [perm % kwargs for perm in self.perms_map[method]]
+
     def has_permission(self, request, view):
+        self._view = view
 
         if hasattr(view, 'perms_map'):
             self.perms_map.update(view.perms_map)
