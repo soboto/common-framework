dc1a4e41212758460c8d12e12038a0dd58505019
Nuno Silva <nmsilva@ubiwhere.pt>
2017-03-31 18:56:56 +0100

    added core test case

---
+++ sbt_common/test/core.py
@@ -0,0 +1,124 @@
+from rest_framework.reverse import reverse
+from rest_framework import status
+from sbt_common.test.authentication import fake_authentication
+from django.utils.six import text_type
+from rest_framework.test import APITestCase
+
+
+class CoreRESTAPITestCase(APITestCase):
+    #: *required*: Base route name of the API endpoints to test.
+    base_name = None
+    #: *required*: The factory class to use for creating the main object to test against.
+    factory_class = None
+    #: Suffix for list endpoint view names. Defaults to ``'-list'``.
+    LIST_SUFFIX = '-list'
+    #: Suffix for detail endpoint view names. Defaults to ``'-detail'``.
+    DETAIL_SUFFIX = '-detail'
+    #: The user instance created if the ``user_factory`` is set and used. Defaults to ``None``.
+    user = None
+    user_type = None
+
+    def get_factory_class(self):
+        """Return the factory class for generating the main object (or model instance) of this test case.
+
+        By default this gets the ``factory_class`` attribute of this class.
+
+        :returns: Factory class used for creating the mock objects.
+        """
+
+        return getattr(self, 'factory_class')
+
+    def create_object(self):
+        factory = self.get_factory_class()
+        return factory.create()
+
+    def setUp(self):
+        self.user = fake_authentication.authenticate_user(self.user_type)
+        fake_authentication.set_user_entity(self.entity_id)
+
+    def tearDown(self):
+        print 'tearDown'
+
+    def get_list_url(self):
+        """Return the list endpoint url.
+
+        :returns: The url of list endpoint.
+        """
+
+        return reverse(self.base_name + self.LIST_SUFFIX)
+
+    def get_detail_url(self, object_id):
+        """Return the detail endpoint url.
+
+        :returns: The url of detail endpoint.
+        """
+        return reverse(self.base_name + self.DETAIL_SUFFIX, args=[text_type(object_id)])
+
+    def get_create_url(self):
+        """Return the create endpoint url.
+
+        :returns: The url of create endpoint.
+        """
+
+        return reverse(self._get_create_name())
+
+    def get_destroy_url(self, object_id):
+        """Return the destroy endpoint url.
+
+        :returns: The url of destroy endpoint.
+        """
+        return reverse(self._get_destroy_name(), args=(self.object_id,))
+
+    def get_update_url(self, object_id):
+        """Return the update endpoint url.
+
+        :returns: The url of update endpoint.
+        """
+        return reverse(self._get_update_name(), args=(self.object_id,))
+
+    def _get_create_name(self):
+        if hasattr(self, 'create_name'):
+            view_name = self.create_name
+        else:
+            view_name = self.base_name + self.LIST_SUFFIX
+
+        return view_name
+
+    def _get_destroy_name(self):
+        if hasattr(self, 'destroy_name'):
+            view_name = self.destroy_name
+        else:
+            view_name = self.base_name + self.DETAIL_SUFFIX
+
+        return view_name
+
+    def _get_update_name(self):
+        if hasattr(self, 'update_name'):
+            view_name = self.update_name
+        else:
+            view_name = self.base_name + self.DETAIL_SUFFIX
+
+        return view_name
+
+
+class ListAPITestCaseMixin(object):
+    pagination_results_field = None
+
+    def get_list_response(self, **kwargs):
+        return self.client.get(self.get_list_url(), **kwargs)
+
+    def _list(self, **kwargs):
+        self.create_object()
+
+        response = self.get_list_response(**kwargs)
+        results = response.data
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK, results)
+
+        if self.pagination_results_field:
+            self.assertIn(self.pagination_results_field, results)
+            results = results[self.pagination_results_field]
+
+        self.assertTrue(len(results) >= 1)
+
+        return response
